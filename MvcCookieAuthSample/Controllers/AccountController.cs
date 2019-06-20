d1f883207550e6b94c0417752c5197ddc599f770
wangliming <wangliming@jingshonline.com>
2019-06-20 09:29:03 +0800

    Initial commit

---
+++ MvcCookieAuthSample/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcCookieAuthSample.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace MvcCookieAuthSample.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public IActionResult MakeLogin()
+        {
+            var claims=new List<Claim>{
+                new Claim(ClaimTypes.Name,"jesse"),
+                new Claim(ClaimTypes.Role,"admin")
+            };
+            var claimIdentity=new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,new ClaimsPrincipal(claimIdentity));
+            return Ok();
+        }
+        public IActionResult Logout()
+        {
+             
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+    }
+}
